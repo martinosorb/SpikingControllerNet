3d0905cb63db9f7478a8272bb7da640f4a914c75
Martino Sorbaro <martino.sorbaro@posteo.net>
2023-02-02 12:07:27 +0100

    working classes

--- ControlledLayer.py
+++ ControlledLayer.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 class ControlledLayer(torch.nn.Module):
@@ -33,5 +34,63 @@ class ControlledLayer(torch.nn.Module):
     def _rate_dynamics(self):
         return torch.sigmoid(self.v)
 
+    def parameters(self, recurse: bool = True):
+        return (self.ff.parameters())
+
     def reset(self):
         self.v = torch.zeros(self.fan_out)
+
+
+class ControlledNetwork(torch.nn.Module):
+    def __init__(self, layers, mode="spiking", leak=0.9, controller_rate=0.1):
+        super().__init__()
+        self.layers = []
+        n_classes = layers[-1]
+        self.controller_rate = controller_rate
+        self.c = torch.zeros(n_classes)
+
+        for fan_in, fan_out in zip(layers[:-1], layers[1:]):
+            self.layers.append(ControlledLayer(fan_in, fan_out, controller_dim=n_classes, mode=mode, leak=leak))
+
+    def forward(self, x, c):
+        for layer in self.layers:
+            x = layer(x, c)
+        return x
+
+    def evolve_controller(self, current_output, control_target_rate):
+        error = control_target_rate - current_output
+        self.c += self.controller_rate * error
+
+    def evolve_to_convergence(self, x, target_rate, control_target_rate, precision=0.01):
+        self.reset()
+        outputs = []
+        controller_effect = []
+
+        while True:
+            output_rate = self(x, self.c)
+            self.evolve_controller(output_rate, control_target_rate)
+            outputs.append(output_rate.numpy())
+            controller_effect.append(self.c.clone().numpy())
+            if abs(output_rate - target_rate) <= precision: break
+
+        outputs = np.asarray(outputs)
+        controller_effect = (outputs - outputs[0])[1:]
+        return outputs, controller_effect
+
+    def reset(self):
+        self.c.zero_()
+        for layer in self.layers:
+            layer.reset()
+
+
+
+# ========
+# for data in dataset:
+#     X, y = data
+#     c = 0.
+
+#     for c < error:
+#         yhat = net(X)
+#         c = net.controller(y, yhat)
+
+#     weight_update()
