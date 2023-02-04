9a698ec15245102b107c4c3cb3a5b783aedadb5c
Martino Sorbaro <martino.sorbaro@posteo.net>
2023-02-04 17:15:49 +0100

    general cleanup of main file

--- deeper.py
+++ deeper.py
@@ -10,7 +10,6 @@ import numpy as np
 v_th = 1
 target_rates = torch.tensor([-1, 1]).float()
 C_precision = 0.01
-timesteps = 2
 initial_steps_plot = 5
 neuron_noise = 0.
 
@@ -29,9 +28,9 @@ net = ControlledNetwork((3, 3, 1), mode="spiking", leak=1., stdp_tau=2.54)
 layer = net.layers[1]
 
 w_evol = []
-control_evol = []
-FF_output_evol = []
-time_to_targ_evol = []
+control_evol = np.zeros(total_points)
+FF_output_evol = np.empty(total_points)
+time_to_targ_evol = np.empty(total_points)
 DW_DH_list = []
 DW_STDP_list = []
 list_output_dynamics = []
@@ -51,10 +50,10 @@ optim = torch.optim.SGD(
 
 # loop over datapoints
 for epoch in range(epochs):
-    for idx in tqdm(range(len(y))):
+    for idx in tqdm(range(total_points)):
         target_rate = y[idx]
         x = X[idx]
-        
+
         control_target_rate = target_rates[target_rate]
         optim.zero_grad()
 
@@ -83,29 +82,23 @@ for epoch in range(epochs):
             next_dyn_plot_idx = (next_dyn_plot_idx + 1) % len(dynamic_plot_idxs)
 
         w_evol.append(layer.ff.weight.data.squeeze().clone().numpy())
-        FF_output_evol.append(output[0])
-        time_to_targ_evol.append(len(input_C))
-
+        FF_output_evol[idx] = output[0]
+        time_to_targ_evol[idx] = len(input_C)
         if len(input_C) > 1:
-            control_evol.append(input_C[-1])
-        else:
-            control_evol.append(0)
+            control_evol[idx] = input_C[-1].item()
+
 
 DW_DH_list = np.asarray(DW_DH_list)
 DW_STDP_list = np.asarray(DW_STDP_list)
 
 # Print acc
 acc = 0
-num_samples = len(y)
-for idx in range(num_samples):
+for idx in range(total_points):
     x = X[idx]
-    target = torch.tensor([y[idx]]).float()
-    # l1 = net.layers[0](x, torch.tensor([0]).float())
-    # out = net.layers[1](l1, torch.tensor([0]).float())
     out = net.feedforward(x)
-    acc += out == target
+    acc += out.item() == y[idx]
 
-print("Accuracy: ", (acc/num_samples).item())
+print("Accuracy: ", (acc / total_points))
 
 
 if plot:
@@ -121,17 +114,15 @@ if plot:
     plt.ylabel("Weights")
 
     plt.subplot(233)
-    max_C = max(control_evol)
-    max_T = max(time_to_targ_evol)
-    control_evol_norm = [c / max_C for (c, s) in zip(control_evol, y) if s == 1]
-    FF_output_evol_norm = [(1 - f) ** 2 for (f, s) in zip(FF_output_evol, y) if s == 1]
-    time_to_output_evol_norm = [t / max_T for (t, s) in zip(time_to_targ_evol, y) if s == 1]
+    control_evol_norm = control_evol / np.max(control_evol)
+    FF_output_evol_norm = (y - FF_output_evol)**2
+    time_to_output_evol_norm = time_to_targ_evol / np.max(time_to_targ_evol)
     plt.plot(control_evol_norm, label="Feedback")
     plt.plot(FF_output_evol_norm, label="MSE Error")
     plt.plot(time_to_output_evol_norm, label="Time to target output")
-    plt.xlabel("Example")
+    plt.xlabel("Sample")
     plt.ylabel("Loss")
-    plt.title("Loss functions for class 1")
+    plt.title("Loss functions")
     plt.legend()
 
     plt.subplot(234)
