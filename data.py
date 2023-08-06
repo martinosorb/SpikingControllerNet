9f8ec68aef4bf98659b4fdbe176cd63a3d2ea9df
efremale <alexsl1@icloud.com>
2023-08-06 03:13:00 +0100

    standardize input data

--- data.py
+++ data.py
@@ -10,11 +10,17 @@ class MNISTDataModule(pl.LightningDataModule):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        self.transform = tr.Compose([tr.ToTensor(), torch.flatten])
+        self.transform = tr.Compose(
+            [
+                tr.ToTensor(),
+                tr.Normalize((0.1307,), (0.3081,)), 
+                torch.flatten
+            ],
+        )
 
     def setup(self, stage: str):
-        self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
-        mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+        self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform, download=True)
+        mnist_full = MNIST(self.data_dir, train=True, transform=self.transform, download=True)
         self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
 
     def train_dataloader(self):
