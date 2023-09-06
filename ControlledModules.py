331fde5ebeb8ead999482f1d50517ce0d2cb791b
efremale <alexsl1@icloud.com>
2023-09-07 00:07:12 +0100

    cuda gpu training setup

--- ControlledModules.py
+++ ControlledModules.py
@@ -35,22 +35,31 @@ class ControlledLayer(torch.nn.Module):
 
         self.stdp_decay = 1 - 1 / tau_stdp if tau_stdp else False
         if self.stdp_decay:
-            self.Apre = torch.empty(batch_size, fan_in + 1)
-            self.Apost = torch.empty(batch_size, fan_out)
+            self.Apre = torch.nn.Parameter(torch.empty(batch_size, fan_in + 1), requires_grad=False)
+            self.Apost = torch.nn.Parameter(torch.empty(batch_size, fan_out), requires_grad=False)
             self.neg_stdp_amplitude = 2. / (1 + alpha_stdp) / tau_stdp
             self.pos_stdp_amplitude = alpha_stdp * self.neg_stdp_amplitude
-
+        self.v = torch.nn.Parameter(
+            torch.zeros((self.batch_size, self.fan_out)),
+            requires_grad=False,
+        )
         self.reset()
 
     def forward(self, inputs, c):
         # Add all-ones column to emulate layer biases
-        inputs = torch.cat((inputs, torch.ones((*inputs.shape[:-1], 1))), dim=-1)
+        inputs = torch.cat(
+            (
+                inputs,
+                torch.ones((*inputs.shape[:-1], 1), device=inputs.device),
+            ),
+            dim=-1,
+        )
 
         ff_input = self.ff(inputs)
         fb_input = self.fb(c)
 
         # LIF dynamics
-        self.v += -self.leak * self.v + ff_input + fb_input
+        self.v.data[:] += -self.leak * self.v + ff_input + fb_input
         outputs = self.dynamics()
 
         if self.stdp_decay:
@@ -59,12 +68,12 @@ class ControlledLayer(torch.nn.Module):
                 output_spikes = spikify(outputs)
             else:
                 input_spikes, output_spikes = inputs, outputs
-            self.Apre = self.Apre * self.stdp_decay + input_spikes.float()
-            self.Apost = self.Apost * self.stdp_decay + output_spikes.float()
+            self.Apre.data[:] = self.Apre * self.stdp_decay + input_spikes.float()
+            self.Apost.data[:] = self.Apost * self.stdp_decay + output_spikes.float()
             neg_outer = torch.einsum("bi,bj->bij", (input_spikes, self.Apost)).transpose(1, 2).mean(dim=0)
             pos_outer = torch.einsum("bi,bj->bij", (output_spikes, self.Apre)).mean(dim=0)
             update = self.pos_stdp_amplitude * pos_outer - self.neg_stdp_amplitude * neg_outer
-            self.ff.weight.grad = self.ff.weight.grad - update
+            self.ff.weight.grad[:] = self.ff.weight.grad - update
 
         return outputs
 
@@ -82,8 +91,8 @@ class ControlledLayer(torch.nn.Module):
             self.Apre.zero_()
             self.Apost.zero_()
         if self.ff.weight.grad is None:
-            self.ff.weight.grad = torch.zeros_like(self.ff.weight)
-        self.v = torch.zeros((self.batch_size, self.fan_out))
+            self.ff.weight.grad = torch.zeros_like(self.ff.weight, device=self.ff.weight.device)
+        self.v.zero_()
 
     @property
     def grad(self):
@@ -107,7 +116,7 @@ class ControlledNetwork(pl.LightningModule):
         self.batch_size = batch_size
         controller_dim = layers[-1]
         self.controller_dim = controller_dim
-        self.c = torch.zeros((batch_size, controller_dim))
+        self.c = torch.nn.Parameter(torch.zeros((batch_size, controller_dim)), requires_grad=False)
 
         for fan_in, fan_out in zip(layers[:-1], layers[1:]):
             layer = ControlledLayer(
@@ -123,7 +132,7 @@ class ControlledNetwork(pl.LightningModule):
 
     def initialize_as_dfc(self):
         # last layer has Q=identity
-        curr_w = torch.eye(self.controller_dim)
+        curr_w = torch.eye(self.controller_dim, device=self.c.device)
         with torch.no_grad():
             for layer in self.layers[::-1]:  # layers backwards
                 layer.fb.weight.data = curr_w
@@ -235,16 +244,17 @@ class EventControllerNet(ControlledNetwork):
         self.max_train_steps = max_train_steps
         self.positive_control = positive_control
 
-    def evolve_controller(self, current_output, one_hot_target):
+    def evolve_controller(self, current_output, one_hot_target, i):
         # this is -1 when there is a spike and it's NOT on the target
         suppressor = current_output * (one_hot_target - 1)
-        self.c += self.controller_rate * suppressor + self.positive_control * one_hot_target
+        update = self.controller_rate * suppressor + self.positive_control * one_hot_target
+        self.c.data[:] += (0.97**i) * update
 
     def evolve_to_convergence(self, x, target):
         self.reset()
         for n_iter in range(self.max_train_steps):
             output = self(x, self.c)
-            self.evolve_controller(output, target)
+            self.evolve_controller(output, target, n_iter)
 
         return n_iter
 
