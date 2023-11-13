e874986bbb786216589fb4f55e3923ef634a2838
Martino Sorbaro <martino.sorbaro@posteo.net>
2023-11-13 19:11:47 +0100

    first changes for conv

--- ControlledModules.py
+++ ControlledModules.py
@@ -8,26 +8,23 @@ def spikify(rate):
     return (rate > torch.rand_like(rate)) * 1.
 
 
-class ControlledLayer(torch.nn.Module):
+class BaseControlledLayer(torch.nn.Module):
     def __init__(
         self,
-        fan_in,
-        fan_out,
         controller_dim,
         batch_size,
         mode="spiking",
         tau_mem=10.,
         tau_stdp=False,
         alpha_stdp=1.0,  # ratio A+/A-
+        bias=True,
     ):
         super().__init__()
 
         self.leak = 1. / tau_mem
         self.threshold = 1.
-        self.fan_out = fan_out
         self.batch_size = batch_size
-        self.ff = torch.nn.Linear(fan_in + 1, fan_out, bias=False)
-        self.fb = torch.nn.Linear(controller_dim, fan_out, bias=False)
+        self.bias = bias
 
         assert mode == "spiking" or mode == "rate"
         self.mode = mode
@@ -35,8 +32,6 @@ class ControlledLayer(torch.nn.Module):
 
         self.stdp_decay = 1 - 1 / tau_stdp if tau_stdp else False
         if self.stdp_decay:
-            self.Apre = torch.nn.Parameter(torch.empty(batch_size, fan_in + 1), requires_grad=False)
-            self.Apost = torch.nn.Parameter(torch.empty(batch_size, fan_out), requires_grad=False)
             self.neg_stdp_amplitude = 2. / (1 + alpha_stdp) / tau_stdp
             self.pos_stdp_amplitude = alpha_stdp * self.neg_stdp_amplitude
         self.v = torch.nn.Parameter(
@@ -47,19 +42,20 @@ class ControlledLayer(torch.nn.Module):
 
     def forward(self, inputs, c):
         # Add all-ones column to emulate layer biases
-        inputs = torch.cat(
-            (
-                inputs,
-                torch.ones((*inputs.shape[:-1], 1), device=inputs.device),
-            ),
-            dim=-1,
-        )
+        if self.bias:
+            inputs = torch.cat(
+                (
+                    inputs,
+                    torch.ones((*inputs.shape[:-1], 1), device=inputs.device),
+                ),
+                dim=-1,
+            )
 
         ff_input = self.ff(inputs)
         fb_input = self.fb(c)
 
         # LIF dynamics
-        self.v.data[:] += -self.leak * self.v + ff_input + fb_input
+        self.v.data += -self.leak * self.v + ff_input + fb_input
         outputs = self.dynamics()
 
         if self.stdp_decay:
@@ -68,13 +64,12 @@ class ControlledLayer(torch.nn.Module):
                 output_spikes = spikify(outputs)
             else:
                 input_spikes, output_spikes = inputs, outputs
-            self.Apre.data[:] = self.Apre * self.stdp_decay + input_spikes.float()
-            self.Apost.data[:] = self.Apost * self.stdp_decay + output_spikes.float()
+            self.Apre.data = self.Apre * self.stdp_decay + input_spikes.float()
+            self.Apost.data = self.Apost * self.stdp_decay + output_spikes.float()
             neg_outer = torch.einsum("bi,bj->bij", (input_spikes, self.Apost)).transpose(1, 2).mean(dim=0)
             pos_outer = torch.einsum("bi,bj->bij", (output_spikes, self.Apre)).mean(dim=0)
             update = self.pos_stdp_amplitude * pos_outer - self.neg_stdp_amplitude * neg_outer
-            self.ff.weight.grad[:] = self.ff.weight.grad - update
-
+            self.ff.weight.grad -= update
         return outputs
 
     def _spiking_dynamics(self):
@@ -97,6 +92,66 @@ class ControlledLayer(torch.nn.Module):
     @property
     def grad(self):
         return self.ff.weight.grad
+    
+
+class ControlledLayer(BaseControlledLayer):
+    def __init__(
+        self,
+        fan_in,
+        fan_out,
+        controller_dim,
+        batch_size,
+        mode="spiking",
+        tau_mem=10.,
+        tau_stdp=False,
+        alpha_stdp=1.0,  # ratio A+/A-
+        bias=True,
+    ):
+        super().__init__(
+            controller_dim=controller_dim,
+            batch_size=batch_size,
+            mode=mode,
+            tau_mem=tau_mem,
+            tau_stdp=tau_stdp,
+            alpha_stdp=alpha_stdp,
+            bias=bias,
+        )
+
+        self.fan_out = fan_out
+        self.ff = torch.nn.Linear(fan_in + int(bias), fan_out, bias=False)
+        self.fb = torch.nn.Linear(controller_dim, fan_out, bias=False)
+
+        if self.stdp_decay:
+            self.Apre = torch.nn.Parameter(torch.empty(batch_size, fan_in + int(bias)), requires_grad=False)
+            self.Apost = torch.nn.Parameter(torch.empty(batch_size, fan_out), requires_grad=False)
+
+    
+
+class ControlledConvLayer(BaseControlledLayer):
+    def __init__(
+        self,
+        conv_layer,
+        controller_dim,
+        batch_size,
+        mode="spiking",
+        tau_mem=10.,
+        tau_stdp=False,
+        alpha_stdp=1.0,  # ratio A+/A-
+    ):
+        super().__init__(
+            fan_in=1, # FIXME this is useless
+            fan_out=1,
+            controller_dim=controller_dim,
+            batch_size=batch_size,
+            mode=mode,
+            tau_mem=tau_mem,
+            tau_stdp=tau_stdp,
+            alpha_stdp=alpha_stdp,
+            bias=False,
+        )
+        self.ff = conv_layer
+
+
 
 
 class ControlledNetwork(pl.LightningModule):
