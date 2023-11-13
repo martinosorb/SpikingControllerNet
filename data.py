cdd58e09a8f828e488b4f926434e25d775beeead
Martino Sorbaro <martino.sorbaro@posteo.net>
2023-11-13 17:07:35 +0100

    updating configs

--- data.py
+++ data.py
@@ -6,10 +6,11 @@ import pytorch_lightning as pl
 
 
 class MNISTDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = "data/", batch_size: int = 32):
+    def __init__(self, data_dir: str = "data/", batch_size: int = 32, num_workers: int = 8):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.num_workers = num_workers
         self.transform = tr.Compose(
             [
                 tr.ToTensor(),
@@ -27,7 +28,7 @@ class MNISTDataModule(pl.LightningDataModule):
         return DataLoader(
             self.mnist_train,
             batch_size=self.batch_size,
-            num_workers=8,
+            num_workers=self.num_workers,
             pin_memory=True,
         )
 
@@ -35,7 +36,7 @@ class MNISTDataModule(pl.LightningDataModule):
         return DataLoader(
             self.mnist_val,
             batch_size=self.batch_size,
-            num_workers=8,
+            num_workers=self.num_workers,
             pin_memory=True,
         )
 
@@ -43,38 +44,26 @@ class MNISTDataModule(pl.LightningDataModule):
         return DataLoader(
             self.mnist_test,
             batch_size=self.batch_size,
-            num_workers=8,
+            num_workers=self.num_workers,
             pin_memory=True,
         )
 
 
 class N_MNISTDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir: str = "data/", batch_size: int = 32):
+    def __init__(self, data_dir: str = "data/", batch_size: int = 32, num_workers: int = 8):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        self.num_workers = 8
-
-    # @staticmethod
-    # def transform_frames(x):
-    #     # frames are TPHW
-    #     print(x.shape)
-    #     x = torch.tensor(x)
-    #     x = torch.moveaxis(x, 2, 0)
-    #     print(x.shape)
-    #     x = torch.flatten(x, start_dim=1)
-    #     print(x.shape)
-    #     raise
-    #     return x
+        self.num_workers = num_workers
 
     def setup(self, stage: str):
         from tonic.datasets import NMNIST
         from tonic.transforms import ToFrame
 
-        # This leads to 32 time bins. For NMNIST, this will mean ~10 ms per bin.
         # Sensor size is 34 and 2 polarities: size is 34*34*2=2312
-        to_frame = ToFrame(sensor_size=NMNIST.sensor_size, n_time_bins=32)
-        transform = tr.Compose([to_frame, torch.tensor, torch.nn.Flatten(start_dim=1)])
+        # With 32 time bins we have ~10 ms per bin (longest recording is 316 ms)
+        to_frame = ToFrame(sensor_size=NMNIST.sensor_size, n_time_bins=100)
+        transform = tr.Compose([to_frame, torch.tensor, torch.nn.Flatten(start_dim=1), lambda x: 1*x])
 
         self.nmnist_test = NMNIST(self.data_dir, train=False, transform=transform)
         nmnist_full = NMNIST(self.data_dir, train=True, transform=transform)
